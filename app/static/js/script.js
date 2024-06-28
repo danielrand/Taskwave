a3355d0ce36388ed8cec8e028e80d1fbc1f4a617
Andrew <Asaganda@gmail.com>
2024-06-27 22:33:02 -0400

    implement textarea input grows as user types

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -44,20 +44,22 @@ function createSubgroup(category) {
   var titleInput = document.createElement('input');
   titleInput.type = 'text';
   titleInput.value = category.category;
+  titleInput.className = 'rounded-t-2xl block -mb-3 h-10 bg-background-dark text-white border-borderColor border-2'
   titleInput.addEventListener('input', function () {
     updateSubgroupTitle(this);
   });
   subgroup.appendChild(titleInput);
 
   var taskList = document.createElement('ul');
-  taskList.className = 'taskList';
+  taskList.className = 'taskList border-borderColor border-2 rounded-b rounded-tr';
   
   var tasks = category.tasks;
   for (var i = 0; i < tasks.length; i++) {
     var task = tasks[i].task;
     var time = tasks[i].time;
     var taskItem = document.createElement('li');
-    taskItem.innerText = task.concat(" (", time, " minutes)");
+    taskItem.className = 'text-white font-inter';
+    taskItem.innerText = `• ${task.concat(" (", time, " minutes)")}`;
     taskList.appendChild(taskItem);
   }
 
@@ -81,4 +83,10 @@ document.getElementById('taskInput').addEventListener('keydown', function(e) {
 })
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('taskInput').value = '• ';
+
+  const textAreaElement = document.getElementById('taskInput');
+  textAreaElement.addEventListener('input', () => {
+    textAreaElement.style.height = 'auto';
+    textAreaElement.style.height = `${textAreaElement.scrollHeight + 10}px`;
+  });
 })
